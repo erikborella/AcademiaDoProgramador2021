41bb2c932528323d6348bd2fdd9503e1db603cdc
Erik Borela <erikborella@gmail.com>
2021-03-17 05:47:32 -0300

    Modelo ChamadaManutencao criado

--- models.py
+++ models.py
@@ -1,6 +1,8 @@
 
 from extensions import db
 
+from datetime import date
+
 class Equipamento(db.Model):
     numeroDeSerie = db.Column(db.String(100), primary_key=True)
 
@@ -9,7 +11,7 @@ class Equipamento(db.Model):
     dataFabricacao = db.Column(db.Date, nullable=False)
     fabricante = db.Column(db.String(100), nullable=False)
 
-    def __init__(self, numeroDeSerie: str, nome: str, precoAquisisao: float, dataFabricacao, fabricante: str):
+    def __init__(self, numeroDeSerie: str, nome: str, precoAquisisao: float, dataFabricacao: date, fabricante: str):
         self.nome = nome
         self.numeroDeSerie = numeroDeSerie
         self.precoAquisisao = precoAquisisao
@@ -18,3 +20,24 @@ class Equipamento(db.Model):
 
     def __repr__(self):
         return "<Equipamento: %r:%r>" % (self.nome, self.precoAquisisao)
+
+
+class ChamadaManutencao(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    titulo = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.String(500), nullable=False)
+
+    equipamento_numeroDeSerie = db.Column(db.String(100), db.ForeignKey('equipamento.numeroDeSerie'), nullable=False)
+    equipamento = db.relationship('Equipamento', backref=db.backref('chamadas', lazy=True))
+
+    dataDeAbertura = db.Column(db.Date, nullable=False)
+
+    def __init__(self, titulo: str, descricao: str, equipamento: Equipamento, dataDeAbertura: date):
+        self.titulo = titulo
+        self.descricao = descricao
+        self.equipamento = equipamento
+        self.dataDeAbertura = dataDeAbertura
+
+    def __repr__(self):
+        return "<Chamada %r:%r>" % (self.titulo, self.equipamento)
