00982ea372a2caa93e5ec25f06ad6e0365cd61f0
Erik Borela <erikborella@gmail.com>
2021-03-18 17:01:12 -0300

    Adicionado view index

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ from app_creator import create_app
 # Cria o servidor flask
 app = create_app()
 
+titulo = "Gerenciador de Equipamento e Manutenções"
+
+@app.route('/')
+def index():
+    return render_template('index.html', titulo=titulo)
+
 # Incia o servidor se o arquivo for executado
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
